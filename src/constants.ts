ac8e2a8530ca3dec19ab8067d207ac7e047e35d0
Akhilesh Patidar <akhileshpatidar989368@gmail.com>
2025-05-27 02:58:48 +0530

    changed constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-const fallbackURL = "https://oss-backend.vercel.app/api/anubhav";
+const fallbackURL = "https://oss-backend-staging.vercel.app/api/anubhav";
 
-export const BACKEND_URL = fallbackURL;
+export const BACKEND_URL = process.env.VITE_BACKEND_URL || fallbackURL;
 export const YOUTUBE_PLAYLIST =
   "https://www.youtube.com/watch?v=c58BoAV2dPc&list=PL4REmU9xh1P14Qv0zmwjve_myZI_rOguU&index=";
