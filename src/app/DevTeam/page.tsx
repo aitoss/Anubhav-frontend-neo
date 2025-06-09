067d173cd609111430d535708e4abf35323269ff
Lokendrakushwah <Lokendrakushwah8051@gmail.com>
2025-06-09 14:23:46 +0530

    fix url

--- src/app/DevTeam/page.tsx
+++ src/app/DevTeam/page.tsx
@@ -100,13 +100,6 @@ const DevTeam = () => {
         },
         {
           id: 6,
-          name: "Akhilesh Patidar",
-          platform: "GitHub",
-          imageSrc: "/dev/akhilesh.jpg",
-          githubId: "Akhileshait",
-        },
-        {
-          id: 7,
           name: "Darshan Garad",
           platform: "GitHub",
           imageSrc: "/dev/darshan.jpeg",
